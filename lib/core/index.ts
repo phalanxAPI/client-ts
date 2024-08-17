11374a737ec71998d2a223097f96147cfefcaa9d
Raj Varsani <varasaniraj5@gmail.com>
2024-08-18 00:43:56 +0530

    fix: update tree logic to handle diff edge cases in express routers
    Co-authored-by: Kshitij Ayush <kshitij4ka@gmail.com>

--- lib/core/index.ts
+++ lib/core/index.ts
@@ -1,12 +1,19 @@
+import { extractRoutes } from "../routes-extractor";
+import { buildRoutingTree } from "../routing-tree";
 import { FormationOptions } from "./types";
 
 export const formation = (options: FormationOptions) => {
   console.log("Initializing Phalanx Formation");
 
-  // TODO: Build Tree
+  const route = extractRoutes(options.app);
+  const routingTree = buildRoutingTree(route);
+  console.log("TREE", routingTree);
+
   // TODO: Push To Phalanx Controller
   // TODO: Continuosly log health checks and send to Phalanx Controller
   // TODO: Add phalanx/status endpoint to parent express app
 
-  console.log(`Phalanx Formation Initialized for App ID: ${options.appId} and Server ID: ${options.serverId}`);
+  console.log(
+    `Phalanx Formation Initialized for App ID: ${options.appId} and Server ID: ${options.serverId}`
+  );
 };
--- lib/routes-extractor/index.ts
+++ lib/routes-extractor/index.ts
@@ -1,15 +1,44 @@
-import type { Express } from 'express';
-import { Route } from '../types';
+import type { Express } from "express";
+import { Route } from "../types";
 
 export const extractRoutes = (app: Express): Route[] => {
-    const routes = app._router.stack
-        .filter((r: any) => r.route)
-        .map((r: any) => {
-            return {
-                method: Object.keys(r.route.methods)[0].toUpperCase(),
-                path: r.route.path,
-            }
-        })
+  const routes: Route[] = [];
 
-    return routes;
-}
+  const split = (thing: any): string[] => {
+    if (typeof thing === "string") {
+      return thing.split("/");
+    } else if (thing.fast_slash) {
+      return [""];
+    } else {
+      const match = thing
+        .toString()
+        .replace("\\/?", "")
+        .replace("(?=\\/|$)", "$")
+        .match(/^\/\^((?:\\[.*+?^${}()|[\]\\\/]|[^.*+?^${}()|[\]\\\/])*)\$\//);
+      return match
+        ? match[1].replace(/\\(.)/g, "$1").split("/")
+        : [`<complex:${thing.toString()}>`];
+    }
+  };
+
+  const print = (path: string[], layer: any) => {
+    if (layer.route) {
+      layer.route.stack.forEach(
+        print.bind(null, path.concat(split(layer.route.path)))
+      );
+    } else if (layer.name === "router" && layer.handle.stack) {
+      layer.handle.stack.forEach(
+        print.bind(null, path.concat(split(layer.regexp)))
+      );
+    } else if (layer.method) {
+      routes.push({
+        path: "/" + path.concat(split(layer.regexp)).filter(Boolean).join("/"),
+        method: layer.method.toUpperCase(),
+      });
+    }
+  };
+
+  app._router.stack.forEach(print.bind(null, []));
+
+  return routes;
+};
