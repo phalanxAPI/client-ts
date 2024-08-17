7b5424876e15446410aefc3d9cfb2501e0f0b89a
Raj Varsani <varasaniraj5@gmail.com>
2024-08-18 01:12:39 +0530

    feat: intercept axios methods
    Co-authored-by: Kshitij Ayush <kshitij4ka@gmail.com>

--- lib/core/index.ts
+++ lib/core/index.ts
@@ -1,3 +1,4 @@
+import { interceptAxiosMethods } from "../intercept-axios-methods";
 import { extractRoutes } from "../routes-extractor";
 import { buildRoutingTree } from "../routing-tree";
 import { FormationOptions } from "./types";
@@ -7,11 +8,9 @@ export const formation = (options: FormationOptions) => {
 
   const route = extractRoutes(options.app);
   const routingTree = buildRoutingTree(route);
-  console.log("TREE", routingTree);
-
   // TODO: Push To Phalanx Controller
+  interceptAxiosMethods(options.axios);
   // TODO: Continuosly log health checks and send to Phalanx Controller
-  // TODO: Add phalanx/status endpoint to parent express app
 
   console.log(
     `Phalanx Formation Initialized for App ID: ${options.appId} and Server ID: ${options.serverId}`
---
+++ lib/intercept-axios-methods/index.ts
@@ -0,0 +1,63 @@
+import type { AxiosRequestConfig, AxiosResponse, AxiosStatic } from "axios";
+
+export const interceptAxiosMethods = (parentAxiosStatic: AxiosStatic) => {
+  const parentGet = parentAxiosStatic.get;
+  const parentPost = parentAxiosStatic.post;
+  const parentPut = parentAxiosStatic.put;
+  const parentDelete = parentAxiosStatic.delete;
+  const parentPatch = parentAxiosStatic.patch;
+
+  parentAxiosStatic.get = async <T = any, R = AxiosResponse<T, any>, D = any>(
+    url: string,
+    config?: AxiosRequestConfig
+  ): Promise<R> => {
+    console.log("GET Request Intercepted");
+    // TODO: Push data to Phalanx Controller
+    return parentGet<T>(url, config) as R;
+  };
+
+  parentAxiosStatic.post = async <T = any, R = AxiosResponse<T, any>, D = any>(
+    url: string,
+    data?: D,
+    config?: AxiosRequestConfig
+  ): Promise<R> => {
+    console.log("POST Request Intercepted");
+    // TODO: Push data to Phalanx Controller
+    return parentPost<T>(url, data, config) as R;
+  };
+
+  parentAxiosStatic.put = async <T = any, R = AxiosResponse<T, any>, D = any>(
+    url: string,
+    data?: D,
+    config?: AxiosRequestConfig
+  ): Promise<R> => {
+    console.log("PUT Request Intercepted");
+    // TODO: Push data to Phalanx Controller
+    return parentPut<T>(url, data, config) as R;
+  };
+
+  parentAxiosStatic.delete = async <
+    T = any,
+    R = AxiosResponse<T, any>,
+    D = any
+  >(
+    url: string,
+    config?: AxiosRequestConfig
+  ): Promise<R> => {
+    console.log("DELETE Request Intercepted");
+    // TODO: Push data to Phalanx Controller
+    return parentDelete<T>(url, config) as R;
+  };
+
+  parentAxiosStatic.patch = async <T = any, R = AxiosResponse<T, any>, D = any>(
+    url: string,
+    data?: D,
+    config?: AxiosRequestConfig
+  ): Promise<R> => {
+    console.log("PATCH Request Intercepted");
+    // TODO: Push data to Phalanx Controller
+    return parentPatch<T>(url, data, config) as R;
+  };
+
+  
+};
