f7f3e015d747cc358bda39e926bdec533c21dd92
Raj Varsani <varasaniraj5@gmail.com>
2024-08-25 23:13:33 +0530

    fix: indentation in files
    Co-authored-by: Kshitij Ayush <kshitij4ka@gmail.com>

--- lib/routing-tree/index.ts
+++ lib/routing-tree/index.ts
@@ -1,43 +1,41 @@
 import { Route, RoutingTree } from "../types";
 
 class RoutingTreeBuilder {
-    _routingTree = {} as RoutingTree;
+  _routingTree = {} as RoutingTree;
 
-    registerRoute(route: Route) {
-        const segments = route.path.split('/').filter(Boolean);
-        let currentMap = this._routingTree;
+  registerRoute(route: Route) {
+    const segments = route.path.split("/").filter(Boolean);
+    let currentMap = this._routingTree;
 
-        segments.forEach((segment) => {
-            if (!currentMap.children) {
-                currentMap.children = {};
-            }
-            if (!currentMap.children[segment]) {
-                currentMap.children[segment] = { methods: {}, children: {} };
-            }
-            currentMap = currentMap.children[segment];
-        });
-
-        if (!currentMap.methods) {
-            currentMap.methods = {};
-        }
-        currentMap.methods[route.method.toLowerCase()] = true;
-    }
-
-    getRoutingTree() {
-        return this._routingTree;
-    }
-}
-
-
-export const buildRoutingTree = (routes: Route[]) => {
-    const mapBuilder = new RoutingTreeBuilder();
-
-    routes.forEach(route => {
-        mapBuilder.registerRoute(route);
+    segments.forEach((segment) => {
+      if (!currentMap.children) {
+        currentMap.children = {};
+      }
+      if (!currentMap.children[segment]) {
+        currentMap.children[segment] = { methods: {}, children: {} };
+      }
+      currentMap = currentMap.children[segment];
     });
 
-    return mapBuilder.getRoutingTree();
+    if (!currentMap.methods) {
+      currentMap.methods = {};
+    }
+    currentMap.methods[route.method.toLowerCase()] = true;
+  }
+
+  getRoutingTree() {
+    return this._routingTree;
+  }
 }
 
+export const buildRoutingTree = (routes: Route[]) => {
+  const mapBuilder = new RoutingTreeBuilder();
+
+  routes.forEach((route) => {
+    mapBuilder.registerRoute(route);
+  });
+
+  return mapBuilder.getRoutingTree();
+};
 
 export default RoutingTreeBuilder;
--- lib/types/index.ts
+++ lib/types/index.ts
@@ -1,9 +1,9 @@
 export type Route = {
-    path: string,
-    method: string,
-}
+  path: string;
+  method: string;
+};
 
 export type RoutingTree = {
-    methods: Record<string, boolean>,
-    children: Record<string, RoutingTree>,
-}
+  methods: Record<string, boolean>;
+  children: Record<string, RoutingTree>;
+};
